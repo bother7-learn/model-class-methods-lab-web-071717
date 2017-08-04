560dfb8ace61774657670d229ee1775b71cd4836
bother7 <github email address>
2017-08-04 18:32:17 -0400

    Done

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    # binding.pry
+  Captain.where(id: Boat.select(:captain_id).where(id: BoatClassification.select(:boat_id).where(classification_id: Classification.where(name: "Catamaran").ids.first)))
+end
+
+  def self.sailors
+    Captain.where(id: Boat.select(:captain_id).where(id: BoatClassification.select(:boat_id).where(classification_id: Classification.where(name: "Sailboat").ids.first)))
+
+  end
+
+  def self.non_sailors
+    Captain.where.not(id: Captain.joins(:boats => :classifications).where('classifications.name' => "Sailboat").ids)
+  end
+
+  def self.talented_seamen
+    
+    Captain.joins(:boats => :classifications).where('classifications.name' => "Sailboat").where(id: Captain.joins(:boats => :classifications).where('classifications.name' => "Motorboat").ids).uniq
+  end
+
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all.uniq
+  end
+
+  def self.longest
+    Classification.where(id: BoatClassification.select(:classification_id).where(id: BoatClassification.where(boat_id: Boat.order(length: :desc).limit(1).ids.first).ids))
+  end
 end
